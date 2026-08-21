0c073357cb3d1faf14521364e8a30744d3c83d47
benksalome-arch <benksalome@gmail.com>
2026-08-21 10:26:16 +0000

    Add account registration route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const authController = require("../controllers/authController");
 
+// Login
 router.post("/login", authController.login);
+
+// Create new account
+router.post("/register", authController.register);
 
 module.exports = router;
