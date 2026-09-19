7c7c6c348a42c7c2cbb738e4c02509ca90e477f2
benksalome-arch <benksalome@gmail.com>
2026-09-19 08:16:54 +0000

    Fix kidding date picker

--- frontend/src/pages/AddKidding.jsx
+++ frontend/src/pages/AddKidding.jsx
@@ -201,34 +201,43 @@ function AddKidding() {
               {t("kiddingDate")}
             </label>
 
-            <input
-              type="text"
-              value={
-                formData.kidding_date
-                  ? formData.kidding_date.split("-").reverse().join("-")
-                  : ""
-              }
-              placeholder="DD-MM-JJJJ"
-              readOnly
-              onClick={() => {
-                const currentValue = formData.kidding_date;
+            <div style={{ position: "relative", width: "100%" }}>
+              <div
+                style={{
+                  ...inputStyle,
+                  width: "100%",
+                  color: formData.kidding_date ? "#222" : "#777",
+                  WebkitTextFillColor: formData.kidding_date ? "#222" : "#777",
+                  pointerEvents: "none",
+                }}
+              >
+                {formData.kidding_date
+                  ? new Date(formData.kidding_date + "T00:00:00").toLocaleDateString("nl-NL", {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                    })
+                  : "DD-MM-JJJJ"}
+              </div>
 
-                const baseDate = currentValue
-                  ? new Date(currentValue + "T00:00:00")
-                  : new Date();
-
-                setCalendarMonth(
-                  new Date(
-                    baseDate.getFullYear(),
-                    baseDate.getMonth(),
-                    1
-                  )
-                );
-
-                setCalendarOpen(true);
-              }}
-              style={inputStyle}
-            />
+              <input
+                type="date"
+                name="kidding_date"
+                value={formData.kidding_date || ""}
+                onChange={handleChange}
+                onClick={(e) => e.currentTarget.showPicker?.()}
+                onFocus={(e) => e.currentTarget.showPicker?.()}
+                required
+                style={{
+                  position: "absolute",
+                  inset: 0,
+                  width: "100%",
+                  height: "100%",
+                  opacity: 0,
+                  cursor: "pointer",
+                }}
+              />
+            </div>
           </div>
 
           {/* PROFESSIONAL CALENDAR */}
