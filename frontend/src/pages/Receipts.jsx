ecfd7794f203a990832df9045353ad4cb4b8162b
benksalome-arch <benksalome@gmail.com>
2026-09-19 22:09:14 +0000

    Fix receipt page text visibility

--- frontend/src/pages/Receipts.jsx
+++ frontend/src/pages/Receipts.jsx
@@ -172,7 +172,7 @@ function Receipts() {
   }
 
   return (
-    <div style={{ width: "100%", maxWidth: "100%", minWidth: 0 }}>
+    <div className="receipts-page" style={{ width: "100%", maxWidth: "100%", minWidth: 0 }}>
       <div
         style={{
           display: "flex",
