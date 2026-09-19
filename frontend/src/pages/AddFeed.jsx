25cd5f01a9fc2449e386da8b97ba5024a21068bd
benksalome-arch <benksalome@gmail.com>
2026-09-19 08:44:30 +0000

    Fix add feed date picker

--- frontend/src/pages/AddFeed.jsx
+++ frontend/src/pages/AddFeed.jsx
@@ -522,35 +522,41 @@ function AddFeed() {
                 {t("purchaseDate")}
               </label>
 
-              <div style={{ position: "relative" }}>
-                <input
-                  id="purchase_date"
-                  type="text"
-                  name="purchase_date"
-                  value={
-                    formData.purchase_date
-                      ? formData.purchase_date.split("-").reverse().join("-")
-                      : ""
-                  }
-                  placeholder="DD-MM-JJJJ"
-                  readOnly
-                  required
-                  onClick={() => {
-                    const selected = formData.purchase_date
-                      ? new Date(formData.purchase_date + "T00:00:00")
-                      : new Date();
-
-                    setCalendarMonth(
-                      new Date(
-                        selected.getFullYear(),
-                        selected.getMonth(),
-                        1
-                      )
-                    );
-                    setCalendarOpen(true);
-                  }}
+              <div style={{ position: "relative", width: "100%" }}>
+                <div
                   style={{
                     ...inputStyle,
+                    width: "100%",
+                    color: formData.purchase_date ? "#222" : "#777",
+                    WebkitTextFillColor: formData.purchase_date ? "#222" : "#777",
+                    pointerEvents: "none",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  {formData.purchase_date
+                    ? new Date(formData.purchase_date + "T00:00:00").toLocaleDateString("nl-NL", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                      })
+                    : "DD-MM-JJJJ"}
+                </div>
+                <input
+                  type="date"
+                  id="purchase_date"
+                  name="purchase_date"
+                  value={formData.purchase_date || ""}
+                  onChange={handleChange}
+                  onClick={(e) => e.currentTarget.showPicker?.()}
+                  onFocus={(e) => e.currentTarget.showPicker?.()}
+                  required
+                  style={{
+                    position: "absolute",
+                    inset: 0,
+                    width: "100%",
+                    height: "100%",
+                    opacity: 0,
                     cursor: "pointer",
                   }}
                 />
